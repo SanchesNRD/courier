b272b6f52999a4d5d2853c294fcfce16f381a009
SanchesNRD <alex2906888@mail.ru>
2021-08-24 12:04:32 +0300

    some changing in log4j.xml and pom.xml

--- src/main/java/by/epam/courierexchange/servlet/HelloServlet.java
+++ src/main/java/by/epam/courierexchange/servlet/HelloServlet.java
@@ -5,9 +5,16 @@ import java.io.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/controller")
 public class HelloServlet extends HttpServlet {
+    final static Logger logger = LogManager.getLogger(HelloServlet.class);
+
+    public HelloServlet() {
+        super();
+    }
 
     public void init() {
 
@@ -16,10 +23,14 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
 
-        String n = request.getParameter("num");
-        int number = Integer.parseInt(n) * 7;
-        request.setAttribute("res", number);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+        HelloServlet.logger.debug("debug level logging supported");
+        response.getWriter().append("Servedat:").append(request.getContextPath());
+        response.getWriter().append("Hello Suraj");
+
+//        String n = request.getParameter("num");
+//        int number = Integer.parseInt(n) * 7;
+//        request.setAttribute("res", number);
+//        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     public void destroy() {
