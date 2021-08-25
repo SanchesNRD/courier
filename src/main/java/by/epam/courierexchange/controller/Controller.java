bad963c92ab6b887042fb4d2481ac654ca8dd217
SanchesNRD <alex2906888@mail.ru>
2021-08-25 10:28:56 +0300

    some changing

--- src/main/java/by/epam/courierexchange/controller/Controller.java
+++ src/main/java/by/epam/courierexchange/controller/Controller.java
@@ -3,7 +3,10 @@ package by.epam.courierexchange.controller;
 import java.io.*;
 import java.util.List;
 
+import by.epam.courierexchange.controller.command.Command;
+import by.epam.courierexchange.controller.command.CommandProvider;
 import by.epam.courierexchange.exception.DaoException;
+import by.epam.courierexchange.model.connection.ConnectionPool;
 import by.epam.courierexchange.model.dao.AddressDao;
 import by.epam.courierexchange.model.dao.impl.AddressDaoImpl;
 import by.epam.courierexchange.model.entity.Address;
@@ -13,21 +16,33 @@ import jakarta.servlet.annotation.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@WebServlet(name = "helloServlet", urlPatterns = "/controller")
+import static by.epam.courierexchange.controller.command.RequestParameter.*;
+
+@WebServlet(urlPatterns = "/controller")
 public class Controller extends HttpServlet {
-    public void init() {
+    private static final Logger logger = LogManager.getLogger();
+    private final CommandProvider COMMAND_PROVIDER = CommandProvider.getInstance();
 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        processRequest(request, response);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-
-        String n = request.getParameter("num");
-        int number = Integer.parseInt(n) * 7;
-        request.setAttribute("res", number);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
     }
 
+    @Override
     public void destroy() {
+        ConnectionPool.getInstance().destroyPool();
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response){
+        String commandName = request.getParameter(COMMAND);
+        Command command = COMMAND_PROVIDER.getCommand(commandName);
     }
 }
--- src/main/java/by/epam/courierexchange/controller/command/Command.java
+++ src/main/java/by/epam/courierexchange/controller/command/Command.java
@@ -1,4 +1,7 @@
 package by.epam.courierexchange.controller.command;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface Command {
+    void execute(HttpServletRequest request);
 }
