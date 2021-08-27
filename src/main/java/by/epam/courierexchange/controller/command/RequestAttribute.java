cf629995ed5773182ebdfbe96ab94c6375444c10
SanchesNRD <alex2906888@mail.ru>
2021-08-27 20:25:49 +0300

    add Password Encryption

---
+++ src/main/java/by/epam/courierexchange/controller/command/RequestAttribute.java
@@ -0,0 +1,10 @@
+package by.epam.courierexchange.controller.command;
+
+public class RequestAttribute {
+    public static final String USER_LOGIN = "user_login";
+    public static final String BANNED_USER = "banned_user";
+    public static final String WRONG_LOGIN_OR_PASSWORD = "wrong_login_or_password";
+    public static final String EXCEPTION = "exception";
+
+    private RequestAttribute(){}
+}
